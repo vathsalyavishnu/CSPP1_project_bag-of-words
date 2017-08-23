7d64404216623411f6bac54b7da376c6df29b5a6
vathsalyavishnu <31200799+vathsalyavishnu@users.noreply.github.com>
2017-08-23 20:24:00 +0530

    Update bag of words.py

--- bag of words.py
+++ bag of words.py
@@ -1,41 +1,60 @@
 import os
 import sys
-import math
 
 def dic(lst):
-	a={}
-	for i in range(len(lst)):
-		a[lst[i]]=lst.count(lst[i])
-	return a
-      
+        a={}
+        l=[]
+        lst1=open(lst,'r')
+        lst1=lst1.read().split(' ')
+        for i in range(0, len(lst1)):
+                lst1[i] = lst1[i].lower().replace("."," ").replace("\n","").replace(",","")
+        for i in range(len(lst1)):
+            if lst1[i] in a:
+                continue
+            else:
+                a[lst1[i]]=lst1.count(lst1[i])
+        return a
+
 def dotproduct(s1,s2):
-	sum=0
-	for i in s1:
-		if i in s2:
-			sum=sum+(s1.get(i)*s2.get(i))
-	return sum
+    sum=0
+    for i in s1:
+        if i in s2:
+            sum=sum+(s1[i]*s2[i])
+    return sum
+
 
 def vectorproduct(s):
-	vec=0
-	for i in s:
-		vec=vec+(s.get(i)**2)
-	norm=(vec)**(1/2)
-	return norm
+    vec=0
+    for i in s:
+        vec=vec+(s[i]**2)
+    norm=(vec)**(1/2)
+    return norm
+    
 
+def plagpercent(a,b):
+        result=(a/b)*100
+        return result
 
-lst1=open("1.txt",'r')
-lst1=lst1.read().split(' ')
-s1=dic(lst1)
-
-lst2=open("2.txt","r")
-lst2=lst2.read().split(" ")
-s2=dic(lst2)
-
-v1=vectorproduct(s1)
-v2=vectorproduct(s2)
-
-num=dotproduct(s1,s2)
-denom=v1*v2
-
-result=(num/(denom))*100
-print("plagarism = ",result,"%")
+n=input("Enter the directory name: ")
+f=[]
+for i in os.listdir(n):
+    if i.endswith(".txt"):
+        f.append(i)
+os.chdir(n)
+lst3=[]
+lst4=[]
+for i in range(len(f)):
+        for j in range(len(f)):
+                if(i==j):
+                     lst4.append(0)
+                else:
+                        lst3.append(dic(f[i]))
+                        print(lst3)
+                        lst3.append(dic(f[j]))
+                        f1=dotproduct(lst3[i],lst3[j])
+                        f2=vectorproduct(lst3[i])
+                        f3=vectorproduct(lst3[j])
+                        f4=f2*f3
+                        lst4.append(plagpercent(f1,f4))
+        print(lst4)
+        lst4=[]
